d7b7f91778ed6d69db41a237965c0a66c4a9d991
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-09-05 00:10:08 +0200

    .

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,40 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ~w[Ace Two Three Four Five Six Seven Eight Nine Ten]
+    suits = ~w[Spades Clubs Hearts Diamonds]
+    
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
 
-  @doc """
-  Hello world.
+  def shuffle(deck) do
+    Enum.shuffle deck
+  end
 
-  ## Examples
+  def contains?(deck, card) do
+    Enum.member? deck, card
+  end
 
-      iex> Cards.hello
-      :world
+  def deal(deck, hand_size) do
+    Enum.split deck, hand_size
+  end
 
-  """
-  def hello do
-    :world
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary deck
+    File.write filename, binary
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _} -> "The file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
